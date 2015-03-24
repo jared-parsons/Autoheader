057f1da4a5de664f995c28fb442ae3473070e135
Jared Parsons <jared@jaredparsons.com>
2015-03-24 15:51:21 -0400

    Added EXPORT directive.

--- Source/AutoHeader.cpp
+++ Source/AutoHeader.cpp
@@ -10,26 +10,58 @@ namespace {
 		output << "#include " << GetValue(token) << "\n"; // thang : put a newline before this?
 	}
 
-	void ParseExportBlock(Tokenizer &tokenizer, std::ostream &output) {
+	void ParseHeaderBlock(Tokenizer &tokenizer, std::ostream &output) {
 		ReadRequiredToken(tokenizer, TokenType::LeftParenthesis);
 		std::size_t parenthesisCount = 1;
+		bool first = true;
 		for (;;) {
 			Maybe<Token> maybeToken = ReadToken(tokenizer);
 			if (!maybeToken.HasValue())
 				throw std::runtime_error("Unexpected end of file.");
 			const Token token = std::move(maybeToken.GetValue());
-			output << token;
 			switch (GetType(token)) {
 				case TokenType::LeftParenthesis:
 					++parenthesisCount;
 					break;
 				case TokenType::RightParenthesis:
 					--parenthesisCount;
-					if (parenthesisCount == 0)
+					if (parenthesisCount == 0) {
+						output << "\n";
 						return;
+					}
 					break;
 				default:
 					break;
+			}
+			if (first) {
+				output << GetValue(token);
+				first = false;
+			} else {
+				output << token;
+			}
+		}
+	}
+
+	void ParseExportBlock(Tokenizer &tokenizer, std::ostream &output) {
+		// thang : what about default parameters?
+		// thang : this assumes braces cannot appear before the body.
+
+		// Skip to first brace.
+		bool first = true;
+		for (;;) {
+			Maybe<Token> maybeToken = ReadToken(tokenizer);
+			if (!maybeToken.HasValue())
+				throw std::runtime_error("Unexpected end of file.");
+			const Token token = std::move(maybeToken.GetValue());
+			if (GetType(token) == TokenType::LeftBrace) {
+				output << ";\n";
+				return;
+			}
+			if (first) {
+				output << GetValue(token);
+				first = false;
+			} else {
+				output << token;
 			}
 		}
 	}
@@ -46,6 +78,8 @@ namespace {
 				if (GetValue(token) == "EXPORT_INCLUDE") {
 					ParseExportInclude(tokenizer, output);
 				} else if (GetValue(token) == "HEADER") {
+					ParseHeaderBlock(tokenizer, output);
+				} else if (GetValue(token) == "EXPORT") {
 					ParseExportBlock(tokenizer, output);
 				}
 			}
